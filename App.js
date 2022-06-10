eff55121a6eaeff163cedfcae5bf3e757cf9be16
GuilhermeCostaDF <guido.costa@globo.com>
2022-06-09 21:26:56 -0300

    ajustando img da pagina de perfil

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, SafeAreaView } from 'react-native';
 
+const colorGithub = '#010409';
+const imageProfileGitHub = 'https://avatars.githubusercontent.com/u/74997292?v=4';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>HELLO, WORLD! THAT'S MY FIRST RN PROJECT</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content} >
+        <Image style={styles.avatar} source={{uri:imageProfileGitHub}} accessibilityLabel='Imagem de perfil do Guilherme Costa' />
+        <StatusBar backgroundColor={colorGithub} barStyle='light-content' />  {/*Serve para manipular a barra de notificações*/}
+      </View>
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
+    backgroundColor: colorGithub,
     justifyContent: 'center',
   },
+
+  content: {
+    alignItems: 'center',
+  },
+
+  avatar: {
+    height: 250,
+    width: 250,
+    borderRadius: 100,
+    borderColor: 'white',
+    borderWidth: 3,
+  }
 });
